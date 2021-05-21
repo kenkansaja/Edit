89ca385b82595cc7b787afac6eb2e4fa8ac8adc9
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-05-21 12:39:25 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 
 class Config(object):
-      BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+      BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
       API_ID = int(os.environ.get("APP_ID", 12345))
       API_HASH = os.environ.get("API_HASH")
       CAPTION_TEXT = os.environ.get("CAPTION_TEXT", "")
