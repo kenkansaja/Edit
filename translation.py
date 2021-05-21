a4dc58ab44cc59896a56536d1e0c3f2f6edd46f4
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-05-21 11:52:36 +0700

    Update translation.py

--- translation.py
+++ translation.py
@@ -4,13 +4,10 @@ class Translation(object):
       START_TEXT = """
 
 😃 Hai {},
-
-I am a simple auto caption bot
-I can automatically add pre-setted caption to the files in channels
-
-I am currently working for a channel if you want to use me, Use my source code and Deploy me
-
-⚜️ Maintained By 👉 @{}
+Saya adalah bot teks otomatis sederhana
+Saya dapat secara otomatis menambahkan teks yang telah diatur sebelumnya ke file di saluran
+Saat ini saya bekerja untuk saluran jika Anda ingin menggunakan saya, Gunakan kode sumber saya dan Terapkan saya
+⚜️ Dikelola Oleh 👉 @ {}
 """    
 
       HELP_TEXT = """
@@ -18,47 +15,15 @@ I am currently working for a channel if you want to use me, Use my source code a
 <u>💡 𝐇𝐞𝐥𝐩</u> 
 
 
-👉 Add me as a Admin in your channel with edit permission
-
-👉 Add your caption in heroku Config Var
+👉  Tambahkan saya sebagai Admin di saluran Anda dengan izin edit
+👉 Tambahkan keterangan Anda di heroku Config Var
    
 <i>(Support Markdown)</i>
+👉 Teruskan file Anda di saluran Anda dan saya akan mengeditnya
 
-👉 Forward your files in your channel and I will edit it
-
-<b><a href="https://t.me/Ts_bots">©️ ᴛʀᴀᴄᴋsᴛᴜᴅɪᴏ's ʙᴏᴛs</a></b>
+<b><a href="https://t.me/kenkanasw">My Owner</a></b>
 """    
-              
-
-     
-      ABOUT_TEXT = """
-
-📕 𝐀𝐛𝐨𝐮𝐭 𝐌𝐞
-
-
-○ ᴍʏ ɴᴀᴍᴇ : [Auto Caption bot](https://t.me/Anylink_Movies)
-
-○ ʟᴀɴɢᴜᴀɢᴇ : ᴘʏᴛʜᴏɴ 
-
-○ ғʀᴀᴍᴇᴡᴏʀᴋ : ᴘʏʀᴏɢʀᴀᴍ 
-
-○ sᴇʀᴠᴇʀ : ʜᴇʀᴏᴋᴜ 
-
-○ ᴠᴇʀsɪᴏɴ : 1.0.0
-
-○ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ  : [ᴛᴏᴜᴄʜ ᴍᴇ 🤗](https://github.com/Ts-bots/AutoCaption-Ts_Bot-Clone)
-
-○ ᴄʀᴇᴀᴛᴏʀ :  [ᴛʀᴀᴄᴋsᴛᴜᴅɪᴏ](https://t.me/trackstudio)
- 
-__ᴘʟᴇᴀsᴇ ɴᴏᴛᴇ ɪ ᴀᴍ ɴᴏᴛ ᴀ ᴘʀᴏ ᴄᴏᴅᴇʀ ᴀɴᴅ ᴛʜɪs ɪs ᴍʏ ғɪʀsᴛ ᴛɪᴍᴇ ɪ ᴀᴍ ɢɪᴠɪɴɢ ᴀɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ᴏғ ʙᴏᴛ ғᴏʀ ᴇᴠᴇʀʏᴏɴᴇ ᴡʜɪᴄʜ ᴡᴀs ᴡʀɪᴛᴇʀ ɪɴ ᴍʏ ᴏᴡɴ ʜᴀɴᴅs, sᴏ ɪғ ᴀɴʏ ʙᴜɢs ғᴇᴇʟ ғʀᴇᴇ ᴛᴏ sᴀʏ
-ɪᴛ ᴍᴀʏ ʙᴇ ɴᴏᴛ ᴛʜᴇ ʙᴇsᴛ ʙᴜᴛ ғᴏʀ ᴍᴇ ɪᴛ ɪs ᴛʜᴇ ʙᴇsᴛ 🥰
-
-ᴛʜᴀɴᴋs ᴛᴏ ᴍʏ ʙᴇʟᴏᴠᴇᴅ ғʀɪᴇɴᴅ [ᴀɴᴏɴʏᴍᴏᴜs](https://t.me/Anoymous_Ns)__
-
-**[©️ ᴛʀᴀᴄᴋsᴛᴜᴅɪᴏ's ʙᴏᴛs](https://t.me/ts_bots)**
-
-"""
-
+   
       MARKDOWN_TEXT = """
 
 🔰 <u>𝐀𝐛𝐨𝐮𝐭 𝐌𝐚𝐫𝐤𝐝𝐨𝐰𝐧</u>
@@ -78,9 +43,9 @@ __ᴘʟᴇᴀsᴇ ɴᴏᴛᴇ ɪ ᴀᴍ ɴᴏᴛ ᴀ ᴘʀᴏ ᴄᴏᴅᴇʀ ᴀ
 
 👉 <b>Hyperlink text</b>
 
-📌 <code>[hyperlink_text](https://t.me/Ts_bots)</code> 
+📌 <code>[Contoh](https://t.me/kenkanasw)</code> 
 
-<b><a href="https://t.me/Ts_bots">©️ ᴛʀᴀᴄᴋsᴛᴜᴅɪᴏ's ʙᴏᴛs</a></b>
+<b><a href="https://t.me/kenkanasw">My Owner</a></b>
 
 """
 
